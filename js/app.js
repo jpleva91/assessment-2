159787201d001ae5b8b043d64e5d3201f8471e5d
jpleva91 <jpleva91@gmail.com>
2017-09-24 14:10:53 -0600

    adds monster gif and morty die function

--- js/app.js
+++ js/app.js
@@ -3,8 +3,32 @@ console.log("js is loaded!"); //Checks jS is loaded
 // Variables //
 var rleft = 0; //Player Rick counter
 var mleft = 0; //Player Morty counter
+var xleft = 0; //CPU Monster counter
 
 // Functions // 
+
+function monsterAttack(){
+	if(rleft < xleft){
+		alert("Morty Died!");
+		$('.morty').css('display', 'none');
+	}
+	if(rleft + mleft === 56){
+		$('#monster').addClass('attack');
+	}
+	if(rleft + mleft === 76){
+		$('#monster').css('left', 10+"em");
+		xleft = 10;
+	}
+	if(rleft + mleft === 96){
+		$('#monster').css('left', 20+"em");
+		xleft = 20;
+	}
+	if(rleft + mleft === 116){
+		$('#monster').css('left', 30+"em");
+		xleft = 30;
+	}
+}
+
 function rickRight(){
 	rleft += 1;
 	$('.rick').css('left', rleft+"em");
@@ -37,11 +61,13 @@ $(document).ready(function(){
 
 		if(e.which==82){
 			rickRight();
+			monsterAttack();
 			checkScore();
 		}
 		if(e.which==77){
 			mortyRight();
 			checkScore();
+			monsterAttack();
 		}
 	})
 });
